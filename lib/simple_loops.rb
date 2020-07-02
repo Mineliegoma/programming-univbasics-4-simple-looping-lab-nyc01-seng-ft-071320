9ce98e6eaacd64f09026e15773c9b48d6ddfcd6a
adept-bell-4492 <garlandegoma@gmail.com>
2020-07-02 15:48:55 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,22 @@
 # Write your methods here
+def loop_message_five_times (string)
+  n=5
+  count = 0 
+  while count <= n do
+    puts string 
+    count+= 1 
+  end 
+end 
+
+def loop_message_n_times(message, number)
+  count=0 
+  n=5
+  loop do 
+    break if count <= n && count >= n
+    puts" number of time the message is output is not equal to the number of integer"
+    
+    count+=1 
+  end 
+  
+end 
+def output_array
